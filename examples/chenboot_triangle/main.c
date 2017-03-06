6a7f356877a14566b63de38ab198231ce538158f
Ben Russell <thematrixeatsyou@gmail.com>
2017-03-06 22:00:57 +1300

    make chenboot_triangle example more robust

--- examples/chenboot_triangle/main.c
+++ examples/chenboot_triangle/main.c
@@ -13,9 +13,26 @@ https://creativecommons.org/publicdomain/zero/1.0/
 #include <stdio.h>
 #include <errno.h>
 
-void gpu_write_gp0(uint32_t v) {
-	//while(((*(volatile uint32_t *)0xBF801814) & (1<<26)) == 0) {
-	//}
+void gpu_write_gp0_command(uint32_t v) {
+	int i;
+
+	for(i = 0; i < 1000; i+=1) {
+		if(((*(volatile uint32_t *)0xBF801814) & (1<<26)) != 0) {
+			break;
+		}
+	}
+
+	*(volatile uint32_t *)0xBF801810 = v;
+}
+
+void gpu_write_gp0_data(uint32_t v) {
+	int i;
+
+	for(i = 0; i < 1000; i+=1) {
+		if(((*(volatile uint32_t *)0xBF801814) & (1<<28)) != 0) {
+			break;
+		}
+	}
 
 	*(volatile uint32_t *)0xBF801810 = v;
 }
@@ -35,23 +52,23 @@ int main(int argc, char *argv[])
 	gpu_write_gp1(0x07000000 | (0xA3-200/2) | ((0xA3+200/2)<<10)); // Yrange
 
 	// Set rendering attribs
-	gpu_write_gp0(0xE1000600); // Dither, allow draw to display, 4bpp tex
-	gpu_write_gp0(0xE2000000); // Tex window setting (don't care)
-	gpu_write_gp0(0xE3000000 | (0) | ((0)<<12)); // Min X/Y
-	gpu_write_gp0(0xE4000000 | (320-1) | ((200-1)<<12)); // Max X/Y
-	gpu_write_gp0(0xE5000000 | (320/2) | ((200/2)<<11)); // Drawing offset
-	gpu_write_gp0(0xE6000000); // Mask bit (draw always)
+	gpu_write_gp0_command(0xE1000600); // Dither, allow draw to display, 4bpp tex
+	gpu_write_gp0_command(0xE2000000); // Tex window setting (don't care)
+	gpu_write_gp0_command(0xE3000000 | (0) | ((0)<<12)); // Min X/Y
+	gpu_write_gp0_command(0xE4000000 | (320-1) | ((200-1)<<12)); // Max X/Y
+	gpu_write_gp0_command(0xE5000000 | (320/2) | ((200/2)<<11)); // Drawing offset
+	gpu_write_gp0_command(0xE6000000); // Mask bit (draw always)
 
 	// Clear screen to colour
-	gpu_write_gp0(0x02402000);
-	gpu_write_gp0((0) | ((0)<<16));
-	gpu_write_gp0((320) | ((200)<<16));
+	gpu_write_gp0_command(0x02402000);
+	gpu_write_gp0_data((0) | ((0)<<16));
+	gpu_write_gp0_data((320) | ((200)<<16));
 
 	// Draw a triangle
-	gpu_write_gp0(0x200080FF);
-	gpu_write_gp0(((60)&0xFFFF) | ((-50)<<16));
-	gpu_write_gp0(((0)&0xFFFF) | ((70)<<16));
-	gpu_write_gp0(((-60)&0xFFFF) | ((-50)<<16));
+	gpu_write_gp0_command(0x200080FF);
+	gpu_write_gp0_data(((60)&0xFFFF) | ((-50)<<16));
+	gpu_write_gp0_data(((0)&0xFFFF) | ((70)<<16));
+	gpu_write_gp0_data(((-60)&0xFFFF) | ((-50)<<16));
 
 	// Enable display
 	gpu_write_gp1(0x03000000); // display on
