3a5932203c88187513788c7f3de0bdb17edcfaa0
Ben Russell <thematrixeatsyou@gmail.com>
2017-03-08 21:32:22 +1300

    bouncy triangle; chenboot fixes

--- examples/chenboot_triangle/main.c
+++ examples/chenboot_triangle/main.c
@@ -16,7 +16,12 @@ https://creativecommons.org/publicdomain/zero/1.0/
 #include <psxregs.h>
 #include <chenboot.h>
 
-void gpu_write_gp0_command(uint32_t v) {
+#define PAIR16(x, y) (((x)&0xFFFF) | ((y)<<16))
+
+volatile uint32_t vblank_counter = 0;
+
+void gpu_write_gp0_command(uint32_t v)
+{
 	int i;
 
 	for(i = 0; i < 1000; i+=1) {
@@ -28,7 +33,8 @@ void gpu_write_gp0_command(uint32_t v) {
 	PSXREG_GPU_GP0 = v;
 }
 
-void gpu_write_gp0_data(uint32_t v) {
+void gpu_write_gp0_data(uint32_t v)
+{
 	int i;
 
 	for(i = 0; i < 1000; i+=1) {
@@ -40,17 +46,48 @@ void gpu_write_gp0_data(uint32_t v) {
 	PSXREG_GPU_GP0 = v;
 }
 
-void gpu_write_gp1(uint32_t v) {
+void gpu_write_gp1(uint32_t v)
+{
 	PSXREG_GPU_GP1 = v;
+}
+
+chenboot_exception_frame_t *isr_main(chenboot_exception_frame_t *sp)
+{
+	// If this isn't an interrupt, spin
+	if((sp->cause&0xFC) != 0) {
+		chenboot_isr_disable();
+		for(;;) {
+		}
+	}
+
+	// Get interrupt flags
+	int iflags = PSXREG_I_STAT;
+
+	// vblank handler
+	if((iflags & (1<<0)) != 0) {
+		vblank_counter += 1;
+	}
+
+	// Acknowledge all interrupts
+	PSXREG_I_STAT = ~iflags;
+
+	// Return
+	return sp;
 }
 
 int main(int argc, char *argv[])
 {
+	int vidy = 0;
+	int x = 0;
+	int y = -45;
+	int vx = 4;
+	int vy = 0;
+
 	// Install ISR handler
 	chenboot_isr_disable();
 	PSXREG_I_MASK = 0x0000;
 	PSXREG_I_STAT = 0x0000;
-	chenboot_isr_install(chenboot_isr_default);
+	chenboot_isr_install(isr_main);
 	chenboot_isr_enable();
 	PSXREG_I_MASK = 0x0001;
 
@@ -61,31 +98,80 @@ int main(int argc, char *argv[])
 	gpu_write_gp1(0x08000009); // PAL, 320x240 base, 15bpp, progressive
 	gpu_write_gp1(0x06000000 | (0x260) | ((0x260+320*8)<<12)); // Xrange
 	gpu_write_gp1(0x07000000 | (0xA3-200/2) | ((0xA3+200/2)<<10)); // Yrange
-
-	// Set rendering attribs
-	gpu_write_gp0_command(0xE1000600); // Dither, allow draw to display, 4bpp tex
-	gpu_write_gp0_command(0xE2000000); // Tex window setting (don't care)
-	gpu_write_gp0_command(0xE3000000 | (0) | ((0)<<12)); // Min X/Y
-	gpu_write_gp0_command(0xE4000000 | (320-1) | ((200-1)<<12)); // Max X/Y
-	gpu_write_gp0_command(0xE5000000 | (320/2) | ((200/2)<<11)); // Drawing offset
-	gpu_write_gp0_command(0xE6000000); // Mask bit (draw always)
+	gpu_write_gp1(0x05000000 | (0) | ((vidy)<<10)); // Output coordinates
 
 	// Clear screen to colour
 	gpu_write_gp0_command(0x02402000);
-	gpu_write_gp0_data((0) | ((0)<<16));
-	gpu_write_gp0_data((320) | ((200)<<16));
-
-	// Draw a triangle
-	gpu_write_gp0_command(0x200080FF);
-	gpu_write_gp0_data(((60)&0xFFFF) | ((-50)<<16));
-	gpu_write_gp0_data(((0)&0xFFFF) | ((70)<<16));
-	gpu_write_gp0_data(((-60)&0xFFFF) | ((-50)<<16));
+	gpu_write_gp0_data(PAIR16(  0,   0+vidy));
+	gpu_write_gp0_data(PAIR16(320, 200+vidy));
 
 	// Enable display
 	gpu_write_gp1(0x03000000); // display on
 
-	// Spin!
+	// Reset vblank counter
+	vblank_counter = 0;
+	uint32_t expected_vblank_counter = 0;
+	uint32_t vblanks = 0;
+
+	// Main loop
 	for(;;) {
+		//
+		// DRAW
+		//
+
+		// Swap vidy
+		vidy = 200-vidy;
+
+		// Set rendering attribs
+		gpu_write_gp0_command(0xE1000600); // Dither, allow draw to display, 4bpp tex
+		gpu_write_gp0_command(0xE2000000); // Tex window setting (don't care)
+		gpu_write_gp0_command(0xE3000000 | (0) | ((0+vidy)<<10)); // Min X/Y
+		gpu_write_gp0_command(0xE4000000 | (320-1) | (((200-1)+vidy)<<10)); // Max X/Y
+		gpu_write_gp0_command(0xE5000000 | (320/2) | (((200/2)+vidy)<<11)); // Drawing offset
+		gpu_write_gp0_command(0xE6000000); // Mask bit (draw always)
+
+		// Clear screen to colour
+		gpu_write_gp0_command(0x02402000);
+		gpu_write_gp0_data(PAIR16(  0,   0+vidy));
+		gpu_write_gp0_data(PAIR16(320, 200));
+
+		// Draw a triangle
+		gpu_write_gp0_command(0x200080FF);
+		gpu_write_gp0_data(PAIR16(( 60)+x, (-50)+y));
+		gpu_write_gp0_data(PAIR16((  0)+x, ( 70)+y));
+		gpu_write_gp0_data(PAIR16((-60)+x, (-50)+y));
+
+		//
+		// PHYSICS
+		//
+
+		// Move
+		for(int i = 0; i < vblanks; i++) {
+			vy += 1;
+			x += vx;
+			y += vy;
+			if(y >= 30) {
+				y = 30*2-y;
+				vy = -vy;
+			}
+			if(x >= 100 && vx > 0) {
+				x = 100*2-x;
+				vx = -vx;
+			} else if(x <= -100 && vx < 0) {
+				x = -100*2-x;
+				vx = -vx;
+			}
+		}
+
+		// Wait for vblank
+		do {
+			vblanks = vblank_counter - expected_vblank_counter;
+		} while(vblanks == 0);
+		expected_vblank_counter += vblanks;
+
+		// Swap buffers
+		gpu_write_gp1(0x05000000 | (0) | ((vidy)<<10)); // Output coordinates
 	}
+
 	return 0;
 }
--- include/chenboot.h
+++ include/chenboot.h
@@ -12,16 +12,17 @@ https://creativecommons.org/publicdomain/zero/1.0/
 typedef struct chenboot_exception_frame {
 	uint32_t epc;
 	uint32_t thread_ra;
+	uint32_t thread_at;
 	uint32_t thread_v[2];
 	uint32_t thread_a[4];
 	uint32_t thread_t[10];
 	uint32_t sr;
 	uint32_t cause;
-} chenboot_exception_frame_t;
+} __attribute__((__packed__)) chenboot_exception_frame_t;
 
 extern chenboot_exception_frame_t *(*chenboot_isr_hook)(chenboot_exception_frame_t *sp);
 chenboot_exception_frame_t *chenboot_isr_default(chenboot_exception_frame_t *sp);
-chenboot_exception_frame_t *chenboot_isr_install(
+void chenboot_isr_install(
 	chenboot_exception_frame_t *(*hook_cb)(
 		chenboot_exception_frame_t *sp));
 void chenboot_isr_enable(void);
