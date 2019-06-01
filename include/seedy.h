9ac198d29081f9855003285f758936977ec4fac1
Ben Russell <thematrixeatsyou@gmail.com>
2019-06-01 22:24:37 +1200

    Initial dump of libseedy

---
+++ include/seedy.h
@@ -0,0 +1,9 @@
+/*
+seedy: CD-ROM driver
+Copyright (C) GreaseMonkey, 2017, licensed under Creative Commons Zero:
+https://creativecommons.org/publicdomain/zero/1.0/
+*/
+
+void seedy_isr_cdrom(void);
+void seedy_init_cdrom(void);
+
--- src/targets.make
+++ src/targets.make
@@ -5,4 +5,5 @@ OUTPUT_LIBS_OBJS =
 include src/chenboot/targets.make
 include src/orelei/targets.make
 include src/sawpads/targets.make
+include src/seedy/targets.make
 
