259956a5719e9ec0a84484484671cf479b51d337
asie <kontakt@asie.pl>
2019-06-03 09:50:20 +0200

    seedy: fix parameter sending, setmode, lba access; add seedy_read_data_sync

--- include/seedy.h
+++ include/seedy.h
@@ -4,6 +4,11 @@ Copyright (C) GreaseMonkey, 2017, licensed under Creative Commons Zero:
 https://creativecommons.org/publicdomain/zero/1.0/
 */
 
+#define SEEDY_READ_WHOLE_SECTORS 1
+#define SEEDY_READ_SINGLE_SPEED 2
+
+int seedy_read_data_sync(int lba, int flags, uint8_t *buffer, int size);
+
 void seedy_isr_cdrom(void);
 void seedy_init_cdrom(void);
 
